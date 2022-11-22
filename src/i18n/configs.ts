88137e0ece1e49a02947e2a898efefca7c61c3ed
chiyukikana <chiyukikana@icloud.com>
2022-11-22 17:15:34 +0800

    feat(lng): add default lng.

--- src/i18n/configs.ts
+++ src/i18n/configs.ts
@@ -16,6 +16,8 @@ const resources = {
 i18next.use(initReactI18next).init({
   resources,
   lng: 'zh',
+  // 当发生错误时默认切换的语言
+  fallbackLng: 'zh',
   interpolation: {
     // 防止字符串xss注入，但是React已经自带了防注入机制，所以这里不再需要打开了。
     escapeValue: false,
